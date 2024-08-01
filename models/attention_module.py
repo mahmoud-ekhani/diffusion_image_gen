ac3120564414302df7efb880f46a985ad1fa9890
Mahmoud Ebrahimkhahni <m.ebrahimkhani1993@gmail.com>
2024-08-01 17:17:57 +0000

    Added Attention class

---
+++ models/attention_module.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+from einops import einsum, rearrange
+
+class Attention(nn.Module):
+    def __init__(self, in_dim, num_heads=4, head_dim=32):
+        super().__init__()
+        self.num_heads = num_heads
+        
+        self.scale = head_dim ** -0.5
+        
+        hidden_dim = num_heads * head_dim
+        
+        self.to_qkv = nn.Conv2d(in_channels=in_dim,
+                                out_channels=hidden_dim * 3,
+                                kernel_size=1,
+                                bias=False)
+        
+        self.to_out = nn.Conv2d(in_channels=hidden_dim,
+                                out_channels=1,
+                                kernel_size=1)
+        
+    def forward(self, x):
+        b, c, h, w = x.shape
+        
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        
+        q, k, v = map(
+            lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h=self.num_heads), 
+            qkv
+        )
+        
+        q = q * self.scale
+        
+        sim = einsum('b h c i, b h c j -> b h i j', q, k)
+        
+        sim = sim - sim.amax(dim=-1, keepdim=True).detach()
+        
+        attn = sim.softmax(dim=-1)
+        
+        out = einsum('b h i j, b h d j -> b h i d', attn, v)
+        
+        out = rearrange(out, 'b h (x y) d -> b (h d) x y', x=h, y=w)
+        
+        return self.to_out(out)
