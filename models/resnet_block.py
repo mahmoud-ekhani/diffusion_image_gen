d12f83801a3ef8c7d1be6b3a351cf6ae2fc2e651
Mahmoud Ebrahimkhahni <m.ebrahimkhani1993@gmail.com>
2024-08-01 15:20:34 +0000

    Updated WeightStandardizedConv2d

--- models/resnet_block.py
+++ models/resnet_block.py
@@ -6,26 +6,19 @@ import torch.nn.functional as F
 from einops import rearrange, reduce
 
 class WeightStandardizedConv2d(nn.Conv2d):
-    def __init__(self, weight, bias, stride, padding, dilation, groups):
-        super().__init__()
-        self.weight = weight
-        self.bias = bias
-        self.stride = stride
-        self.padding = padding
-        self.dilation = dilation
-        self.groups = groups
-    
+    """
+    https://arxiv.org/abs/1903.10520
+    weight standardization purportedly works synergistically with group normalization
+    """
+
     def forward(self, x):
         eps = 1e-5 if x.dtype == torch.float32 else 1e-3
-        
+
         weight = self.weight
-        
-        mean = reduce(weight, 'o ... -> o 1 1 1', 'mean')
-        
-        var = reduce(weight, 'o ... -> o 1 1 1', partial(torch.var, unbiased=False))
-        
-        normalized_weight = (weight - mean) / (var + eps).sqrt()
-        
+        mean = reduce(weight, "o ... -> o 1 1 1", "mean")
+        var = reduce(weight, "o ... -> o 1 1 1", partial(torch.var, unbiased=False))
+        normalized_weight = (weight - mean) / (var + eps).rsqrt()
+
         return F.conv2d(
             x,
             normalized_weight,
