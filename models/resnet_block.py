323727f642d75062b887cb4a941f5342d76b7ed4
Mahmoud Ebrahimkhahni <m.ebrahimkhani1993@gmail.com>
2024-08-01 15:47:18 +0000

    Added ResnetBlock

--- models/resnet_block.py
+++ models/resnet_block.py
@@ -58,6 +58,44 @@ class Block(nn.Module):
         
         return x
         
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, dim_out, *, time_emb_dim=None, groups=8):
+        super().__init__()
+        self.mlp = (
+            nn.Sequential(nn.SiLU(),
+                          nn.Linear(in_features=time_emb_dim,
+                                    out_features=dim_out * 2)) if exists(time_emb_dim)
+            else None
+        )
+        
+        self.block1 = Block(dim=dim,
+                            dim_out=dim_out,
+                            groups=groups)
+        
+        self.block2 = Block(dim=dim_out,
+                            dim_out=dim_out,
+                            groups=groups)
+        
+        self.res_conv = nn.Conv2d(in_channels=dim,
+                                  out_channels=dim_out,
+                                  kernel_size=1) if dim != dim_out else nn.Identity()
+        
+        def forward(self, x, time_emb=None):
+            scale_shift = None
+            
+            if exists(self.mlp) and exists(time_emb):
+                time_emb = self.mlp(time_emb)
+                
+                time_emb = rearrange(time_emb, 'b c -> b c 1 1')
+                
+                scale_shift = time_emb.chunk(2, dim=1)
+                
+            h = self.block1(x, scale_shift=scale_shift)
+            h = self.block2(h)
+            
+            return h + self.res_conv(h)
+                
+                
         
         
         
